53377d55b986cd266b60bdf1bc72d14c6f2b2d71
Arkadeion <arkadeion@gmail.com>
2022-08-19 14:34:32 +0200

    Add searching and query parameter support

--- src/Markets.js
+++ src/Markets.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { DataContext } from "./DataContext";
 import DataTable from "react-data-table-component";
+import { useLocation } from "react-router";
 
 const columns = [
   {
@@ -25,10 +26,55 @@ export function Markets() {
 
   const data = useContext(DataContext);
 
+  const query = useLocation().search;
+
+  const [searchInput, setSearchInput] = useState('');
+
+  const [selectFilter, setSelectFilter] = useState('symbol');
+
+  const [queryStatus, setQueryStatus] = useState(false);
+
+  function handleInput(event) {
+    setSearchInput(event.target.value);
+  }
+
+  function handleSelect(event) {
+    setSelectFilter(event.target.value);
+  }
+
+  function handleFilter() {
+
+    let listCopy;
+
+    if (sessionStorage.getItem('data') === null) {
+
+      sessionStorage.setItem('data', JSON.stringify(data.dataArray));
+      listCopy = JSON.parse(sessionStorage.getItem('data'));
+    } else {
+      listCopy = JSON.parse(sessionStorage.getItem('data'));
+    }
+
+    if (searchInput === '') {
+
+      data.setDataArray([...listCopy])
+
+    } else {
+
+      const filteredList = listCopy.filter((obj) => obj[selectFilter].match(new RegExp(searchInput, 'i')))
+
+      data.setDataArray([...filteredList]);
+    }
+  }
+
+  function handleReset() {
+
+    let listCopy = JSON.parse(sessionStorage.getItem('data'));
+    data.setDataArray([...listCopy]);
+    setSearchInput('')
+  }
+
   function orderTable(event) {
-
     const value = event.target.value;
-
     switch (value) {
       case 'ascending':
         data.setDataArray(data.dataArray.sort((a, b) => (a.symbol > b.symbol) ? 1 : ((b.symbol > a.symbol) ? -1 : 0)));
@@ -41,27 +87,56 @@ export function Markets() {
       default:
         break;
     }
-
   }
+
+  function handleQuery() {
+
+    const baseAsset = new URLSearchParams(query).get('base_asset');
+
+    if (baseAsset) {
+      setSearchInput(baseAsset);
+      setSelectFilter('baseAsset');
+    }
+  }
+
+  useEffect(() => {
+    handleQuery();
+    setQueryStatus(true);
+  }, [query])
+
+  useEffect(() => {
+    handleFilter();
+  }, [queryStatus])
 
   return (
     <div>
+      <div className="flex mb-5 justify-between bg-white rounded-xl border-2 border-black w-3/5 p-4 mt-8 mb-8">
+        <select className="border-black border-2 mr-2" onChange={orderTable}>
+          <option>Order by Name</option>
+          <option value="ascending">Ascending</option>
+          <option value="descending" >Descending</option>
+        </select>
+        <div>
+          <select className="border-black border-2 mr-2" value={selectFilter} onChange={handleSelect}>
+            <option value="symbol">Market</option>
+            <option value="baseAsset" >Base Asset</option>
+            <option value="quoteAsset" >Quote Asset</option>
+          </select>
+          <input className="border-black border-2 mr-2" value={searchInput} onChange={handleInput} />
+          <button className="border-black border-2 mr-2" onClick={handleFilter} >Search</button>
+          <button className="border-black border-2 mr-2" onClick={handleReset} >Reset</button>
+        </div>
+      </div>
       <div className="bg-white rounded-xl border-2 border-black w-3/5 p-4 mb-8">
         {data.isLoadingMarkets && <h1 className="text-3xl font-bold" >Loading...</h1>}
         {data.error && <h1 className="text-xl font-bold" >{data.error.message}</h1>}
         {/* {!data.error && data.dataArray && <ul>{data.dataArray.map((symbol) => <li key={symbol.id}>Market: {symbol.symbol} --- Price: {symbol.price} --- Base Asset: {symbol.baseAsset} --- Quote Asset: {symbol.quoteAsset} --- ID: {symbol.id}</li>)}</ul>} */}
         {!data.error && data.dataArray &&
           <div>
-            <select onChange={orderTable}>
-              <option>Order by Name</option>
-              <option value="ascending">Ascending</option>
-              <option value="descending" >Descending</option>
-            </select>
-            <DataTable
+            <DataTable pagination={true} paginationPerPage='30'
               columns={columns}
               data={data.dataArray} />
           </div>
-
         }
       </div>
     </div>
--- src/useFetchApi.js
+++ src/useFetchApi.js
@@ -1,71 +1,5 @@
 import { useEffect, useState } from "react";
 
-/* const fetcher = (url) => fetch(url).then((response) => response.json());
-
-export function useFetchApi() {
-
-    const priceArray = useSWR('https://api.binance.com/api/v3/ticker/price', fetcher, {
-        revalidateOnFocus: false,
-        revalidateOnMount: true,
-        revalidateOnReconnect: false,
-        refreshWhenOffline: false,
-        refreshWhenHidden: false,
-        refreshInterval: 15000
-    });
-
-    const exchangeArray = useSWR('https://api.binance.com/api/v3/exchangeInfo', fetcher, {
-        revalidateOnFocus: false,
-        revalidateOnMount: true,
-        revalidateOnReconnect: false,
-        refreshWhenOffline: false,
-        refreshWhenHidden: false,
-        refreshInterval: 15000
-    });
-
-    const [priceData, setPriceData] = useState();
-
-    const [exchangeData, setExchangeData] = useState();
-
-    const [dataArray, setDataArray] = useState();
-
-    useEffect(() => {
-        if (priceArray) {
-            setPriceData(() => priceArray.data);
-        }
-
-        if (exchangeArray) {
-            setExchangeData(() => exchangeArray.data)
-        }
-
-        if (priceData && exchangeData) {
-            const mappedArray = priceArray.data.map(ticker => {
-
-                const find = exchangeArray.data.symbols.find(name => name.symbol === ticker.symbol);
-
-                return {
-                    ...ticker,
-                    baseAsset: find.baseAsset,
-                    quoteAsset: find.quoteAsset,
-                }
-            });
-
-            setDataArray(mappedArray);
-        }
-
-    }, [])
-
-    return (
-        {
-            priceData: priceData,
-            exchangeData: exchangeData,
-            dataArray: dataArray,
-            isLoadingAssets: !exchangeArray.data && !exchangeArray.error,
-            isLoadingMarkets: !dataArray && !exchangeArray.error && !priceArray.error,
-            error: priceArray.error || exchangeArray.error,
-        }
-    )
-} */
-
 export function useFetchApi() {
 
 
@@ -75,16 +9,16 @@ export function useFetchApi() {
 
         if (!JSON.parse(sessionStorage.getItem('data'))) {
 
-        Promise.all([
-            fetch('https://api.binance.com/api/v3/ticker/price').then(response => response.json()),
-            fetch('https://api.binance.com/api/v3/exchangeInfo').then(response => response.json())
-        ]).then(([priceResponse, exchangeResponse]) => {
-            mapArray(priceResponse, exchangeResponse.symbols);
-        })
-        .then(() => sessionStorage.setItem('data', JSON.stringify(dataArray)));
-    } else {
+            Promise.all([
+                fetch('https://api.binance.com/api/v3/ticker/price').then(response => response.json()),
+                fetch('https://api.binance.com/api/v3/exchangeInfo').then(response => response.json())
+            ]).then(([priceResponse, exchangeResponse]) => {
+                mapArray(priceResponse, exchangeResponse.symbols);
+            })
+        }
+
         setDataArray(JSON.parse(sessionStorage.getItem('data')));
-    }
+
     }, [])
 
     function mapArray(priceData, exchangeData) {
@@ -102,10 +36,10 @@ export function useFetchApi() {
         });
 
         setDataArray(mappedArray);
-
+        sessionStorage.setItem('data', JSON.stringify(mappedArray))
     }
 
-    return({
+    return ({
         dataArray: dataArray,
         isLoadingMarkets: !dataArray,
         setDataArray: setDataArray,
